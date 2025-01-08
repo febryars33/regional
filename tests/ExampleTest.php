14deeacb562d21b4c951bae88d015b6e31a250f8
Febriansyah Riki Setiadi <48888849+febryars33@users.noreply.github.com>
2025-01-09 01:26:34 +0700

    update: removing get records

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,15 +1,5 @@
 <?php
 
-use Snairbef\Regional\Models\Province;
-
-// it('can test', function () {
-//     expect(true)->toBeTrue();
-// });
-
-it('get province records', function () {
-    Province::factory()->create();
-
-    $province = Province::all();
-
-    expect($province->count())->toBe(1);
+it('can test', function () {
+    expect(true)->toBeTrue();
 });
